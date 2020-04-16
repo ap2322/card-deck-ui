90d9469596eb8816c82e887acf4cd378d69363ad
Alice Post <alicep2322@gmail.com>
2020-04-15 22:24:46 -0600

    Add game-start component

---
+++ src/app/games/game-start/game-start.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-game-start',
+  templateUrl: './game-start.component.html',
+  styleUrls: ['./game-start.component.css']
+})
+
+export class GameStartComponent {
+  welcome = "Start Game"
+}
