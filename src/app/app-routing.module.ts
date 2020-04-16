08116059c62ef7d4dd549c13650829de69573eb0
Alice Post <alicep2322@gmail.com>
2020-04-15 23:52:09 -0600

    Create game-show comp and add to router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+
 import { GameStartComponent } from "./games/game-start/game-start.component";
+import { GameShowComponent } from "./games/game-show/game-show.component";
 
 const routes: Routes = [
-  { path: '', component: GameStartComponent }
+  { path: '', component: GameStartComponent },
+  { path: 'deal', component: GameShowComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GameStartComponent } from './games/game-start/game-start.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { GameShowComponent } from './games/game-show/game-show.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GameStartComponent
+    GameStartComponent,
+    GameShowComponent
   ],
   imports: [
     BrowserModule,
